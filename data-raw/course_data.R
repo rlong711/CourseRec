6d7a635fa409d7e9c76dab2686b96b9ab8d5d51b
Mattea29 <mwhitlow@smith.edu>
2023-11-24 15:01:55 -0500

    starting outline for scraping

--- data-raw/course_data.R
+++ data-raw/course_data.R
@@ -2,3 +2,15 @@
 # Smith website
 
 # hi this is Mattea
+library(tidyverse)
+library(rvest)
+
+url <- "https://www.smith.edu/academics/programs-courses/course-search"
+
+course_info <- read_html(url)
+course_info
+
+course_info <- course_info |>
+  html_attr("")
+course_info
+
