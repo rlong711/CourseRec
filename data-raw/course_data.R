13f30892a8c9a77b99ad0ab6cae51d103f18859b
Hebe Guo <zguo@smith.edu>
2023-11-27 15:38:47 -0500

    1

--- data-raw/course_data.R
+++ data-raw/course_data.R
@@ -1,2 +1,13 @@
 # This contains the script for collecting the course catalog information from the
 # Smith website
+
+
+# This is Hebe
+library(tidyverse)
+library(rvest)
+
+courses <- read_html("https://www.smith.edu/apps/course_search/")
+
+course_dept <- courses %>%
+  html_elements(css = "span.course-dept") %>%
+  html_text()
